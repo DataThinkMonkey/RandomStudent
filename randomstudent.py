668d3b061abe30e780d0c1357861c2482d721113
Jared Bernard <jared@Jareds-MBP.netgear.com>
2018-01-26 23:53:18 -0600

    Prompts user input for file name. Random selects name. Prompts to select another name. While loop until option is not 1.

--- randomstudent.py
+++ randomstudent.py
@@ -1,3 +1,17 @@
 import random
-studentlist = ['Robin', 'Billy', 'Garrett', 'Wyatt', 'Becky']
-print(random.choice(color_list))
+
+list = input("Enter file name of list in current directory: ")
+
+# Read student list files as list, split by line.
+with open(list, 'r') as f:
+    studentlist = f.read().splitlines()
+
+# Randomly choose from list
+print(random.choice(studentlist))
+
+# Ask for another random name
+repeat = int(input("\n1. List another random name.\n2. Quit.\nSelection: "))
+
+while repeat==1:
+    print(random.choice(studentlist))
+    repeat = int(input("\n1. List another random name.\n2. Quit.\nSelection:  "))
